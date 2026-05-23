21dd1e4265d02eddd800b8f41c8ac9962b9539a0
pa526 <p7266634@gmail.com>
2026-05-24 02:23:00 +0530

    Added verification code route

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,9 @@ export async function middleware(request: NextRequest) {
     ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-    // return NextResponse.redirect(new URL('/home', request.url));
+    if(!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
 }
 
 export const config = {
