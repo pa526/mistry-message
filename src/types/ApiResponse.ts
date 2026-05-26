150a3eb7a75497b34d196ab93bdf2136415fa1a4
pa526 <p7266634@gmail.com>
2026-05-27 02:33:50 +0530

    Added frontend logic using shadcn

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,9 +1,11 @@
 import { Message } from "@/models/User";
 
 
-export interface ApiResponse {
+interface ApiResponse {
     success: boolean;
     message: string;
     isAcceptingMessages?: boolean;
     messages?: Array<Message>
 }
+
+export default ApiResponse;
